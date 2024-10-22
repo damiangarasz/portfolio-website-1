465aeec9e5c0f5039ccb939c7633503ecb54ab9b
damiangarasz <damian.garasz@gmail.com>
2024-10-22 17:11:47 +0200

    totalny mess ale w dobrą stronę

--- JS/search.js
+++ JS/search.js
@@ -1,7 +1,9 @@
 // logika odpowiadająca za działanie search bara jeszcze nie gotowa
 //TODO
+const form = document.querySelector(".search-bar");
+const searchBarChwytak = document.querySelector(".search-wrapper");
+let interval;
 export function search() {
-  const form = document.querySelector(".search-bar");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -21,7 +23,7 @@ export function searchToggle() {
     elArr.forEach((el) => {
       el.dataset.state = "on";
     });
-
+    searchBarImput.focus();
     event.stopPropagation();
     smoothBarGrow();
   });
@@ -32,49 +34,36 @@ export function searchToggle() {
         el.dataset.state = "off";
       });
       //na twardo ustawiam spowrotem poczatkowy rozmiar search bara
-      const searchBarChwytak = document.querySelector(".search-wrapper");
       searchBarChwytak.style.width = "50px";
+      clearInterval(interval);
     }
   });
 }
 
-//dynamimczna szerokość search bara/wrappera i umieszczenie arrow buttona plus animacja otwierania
-
-const searchBarChwytak = document.querySelector(".search-wrapper");
-let szerokoscOkna = window.innerWidth;
-const szerokoscDocelowa = szerokoscOkna * 0.1;
-
-setInterval(sprAktualnegoRozmiaruOkna, 1100);
-
-function sprAktualnegoRozmiaruOkna() {
-  szerokoscOkna = window.innerWidth;
-  console.log(szerokoscOkna);
-}
-
-// searchBarChwytak.style.width = szerokoscOkna * 0.1 + "px";
-
 function smoothBarGrow() {
-  let aktualnaSerokosc = 50;
-  //to linia, która wymusza przerysowanie elementu w przeglądarce.
-  //Oznacza to, że przeglądarka będzie musiała ponownie obliczyć styl elementu, zanim zacznie stosować jakiekolwiek inne zmiany stylu.
-  searchBarChwytak.offsetWidth;
+  //sprawdzanie szerokości okna podczas załadowania storny
+  let aktualnaSzerokoscOkna = window.innerWidth;
+  let poczatkowaSzerokoscSearchBara = 50;
+  let docelowaSzerokoscSearchBara = aktualnaSzerokoscOkna * 0.1;
+  //loop sprawdzający szeokość okna co zadaną ilaść czasu
+  interval = setInterval(sprAktualnejSzerokosciOkna, 500);
 
-  //metoda na możliwośc dodania wyłącznika setInterval
-  let intervalID = setInterval(powiekszanieBara, 1);
-
-  function powiekszanieBara() {
-    searchBarChwytak.style.width = aktualnaSerokosc + "px";
-    aktualnaSerokosc++;
+  function sprAktualnejSzerokosciOkna() {
+    aktualnaSzerokoscOkna = window.innerWidth;
+    docelowaSzerokoscSearchBara = aktualnaSzerokoscOkna * 0.1;
+    console.log(docelowaSzerokoscSearchBara);
   }
 
-  setInterval(zatrzymanieIntervala, 100);
+  requestAnimationFrame(animacjaSearchBara);
 
-  function zatrzymanieIntervala() {
-    if (szerokoscDocelowa <= aktualnaSerokosc) {
-      clearInterval(intervalID);
-    } else {
-      //tutaj gdy zwiększe okno poinnno przeliczyć pasek jeszcze raz
-      powiekszanieBara();
+  function animacjaSearchBara() {
+    if (poczatkowaSzerokoscSearchBara < docelowaSzerokoscSearchBara) {
+      searchBarChwytak.style.width = poczatkowaSzerokoscSearchBara + "px";
+      poczatkowaSzerokoscSearchBara += 4;
+    } else if (poczatkowaSzerokoscSearchBara >= docelowaSzerokoscSearchBara) {
+      cancelAnimationFrame(animacjaSearchBara);
     }
+
+    requestAnimationFrame(animacjaSearchBara);
   }
 }
